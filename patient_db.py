4ef24c81248437fee61bc551e7566775344aa0e4
mehakkhandelwal142006-sketch <mehakkhandelwal142006@gmail.com>
2026-03-16 20:46:17 +0530

    Update patient_db.py

--- patient_db.py
+++ patient_db.py
@@ -1,11 +1,11 @@
 import sqlite3
 import pandas as pd
 
-DB_NAME = "patients.db"
 
+# ───────── CREATE TABLE ─────────
 def create_table():
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect("patients.db")
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -14,7 +14,9 @@ def create_table():
         name TEXT,
         age INTEGER,
         gender TEXT,
-        contact TEXT
+        contact TEXT,
+        blood_group TEXT,
+        registered_by TEXT
     )
     """)
 
@@ -22,41 +24,57 @@ def create_table():
     conn.close()
 
 
-def add_patient(pid, name, age, gender, contact):
+# ───────── ADD PATIENT ─────────
+def add_patient(pid, pname, age, gender, contact, blood, registered_by):
 
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
+    try:
+        conn = sqlite3.connect("patients.db")
+        cursor = conn.cursor()
 
-    cursor.execute(
-        "INSERT INTO patients VALUES (?, ?, ?, ?, ?)",
-        (pid, name, age, gender, contact)
-    )
+        cursor.execute("""
+        INSERT INTO patients
+        (patient_id, name, age, gender, contact, blood_group, registered_by)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """, (pid, pname, age, gender, contact, blood, registered_by))
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+        conn.close()
+
+        return True, None
+
+    except sqlite3.IntegrityError:
+        return False, "Patient ID already exists"
 
 
+# ───────── GET ALL PATIENTS ─────────
 def get_all_patients():
 
     conn = sqlite3.connect("patients.db")
 
-    df = pd.read_sql_query("SELECT * FROM patients", conn)
+    df = pd.read_sql_query("""
+        SELECT
+        patient_id AS "Patient ID",
+        name AS "Name",
+        age AS "Age",
+        gender AS "Gender",
+        contact AS "Contact",
+        blood_group AS "Blood Group",
+        registered_by AS "Registered By"
+        FROM patients
+    """, conn)
 
     conn.close()
 
     return df
 
-def delete_patient(patient_id):
 
-    import sqlite3
+# ───────── DELETE PATIENT ─────────
+def delete_patient(patient_id):
 
     conn = sqlite3.connect("patients.db")
     cursor = conn.cursor()
 
-    cursor.execute(
-        "DELETE FROM patients WHERE patient_id = ?",
-        (patient_id,)
-    )
+    cursor.execute("DELETE FROM patients WHERE patient_id = ?", (patient_id,))
 
     conn.commit()
     conn.close()
